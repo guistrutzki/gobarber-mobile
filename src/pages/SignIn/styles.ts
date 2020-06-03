0c0d1cb3894678c06207b284261afee5d3046822
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2020-06-03 02:26:21 -0300

    :sparkles: Add customs fonts

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View({});
+export const Container = styled.View({
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const Title = styled.Text({
+  fontSize: 24,
+  color: '#f4ede8',
+  fontFamily: 'RobotoSlab-Medium',
+  marginTop: 64,
+  marginBottom: 24,
+});
