1251d134a61470a86d5a9fcf68b1b50bcfd2ba26
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2020-06-03 02:02:48 -0300

    :sparkles: Add app navigation setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+  </Auth.Navigator>
+);
+
+const Routes: React.FC = () => (
+  <NavigationContainer>
+    <AuthRoutes />
+  </NavigationContainer>
+);
+
+export default Routes;
